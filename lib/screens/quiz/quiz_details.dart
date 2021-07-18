bb51291968b2522df6ac28ac741b0323a5ffcdac
Alex <alexandre.marleau2000@gmail.com>
2021-07-18 14:02:32 -0400

    Quiz Details screen non fini

---
+++ lib/screens/quiz/quiz_details.dart
@@ -0,0 +1,146 @@
+import 'package:demo3/custom_painter/bg_circles.dart';
+import 'package:demo3/model/quiz.dart';
+import 'package:demo3/screens/welcome_screen/widgets/buttons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class QuizDetailsPage extends StatelessWidget {
+  final Quiz quiz;
+
+  QuizDetailsPage({
+    Key? key,
+    required this.quiz,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Stack(
+            children: <Widget>[
+              // Positioned(
+              //   bottom: -600,
+              //   right: -350,
+              //   height: 620,
+              //   child: CustomPaint(
+              //     size: Size(370, (360 * 1.6666666666666667).toDouble()),
+              //     painter: RPSCustomPainter180(),
+              //   ),
+              // ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: MediaQuery.of(context).size.height / 2.5,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.deepOrange,
+                          Colors.orangeAccent.shade400,
+                        ],
+                      )),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 50),
+                          Text(
+                            quiz.quizTitle,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                          SizedBox(height: 150),
+                          IntrinsicHeight(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  width: 100,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.timer,
+                                        color: Colors.white,
+                                        size: 25.0,
+                                      ),
+                                      Text(" " + quiz.timelimit,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold)),
+                                      Text(" min",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold)),
+                                    ],
+                                  ),
+                                ),
+                                const VerticalDivider(
+                                  color: Colors.white,
+                                  thickness: 1.5,
+                                  width: 20,
+                                ),
+                                Container(
+                                  width: 100,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(quiz.noOfQuestions,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold)),
+                                      Text(" Questions",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold)),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.all(20),
+                          child: Text("Instructions", style: TextStyle(fontSize: 15),),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 1.2,
+                          height: MediaQuery.of(context).size.height / 3,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.orange,
+                              width: 2,
+                            ),
+                          ),
+                          child: Text(quiz.instructions, ),
+                        ),
+                        LoginButton(onPressed: () {}, text: "Start"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
