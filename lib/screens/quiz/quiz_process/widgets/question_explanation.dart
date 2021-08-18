b343526c2064211290e32d779c0fa61318312a75
Alex <alexandre.marleau2000@gmail.com>
2021-08-18 14:27:47 -0400

    Mulitple choice scrollable et Question Explanation

---
+++ lib/screens/quiz/quiz_process/widgets/question_explanation.dart
@@ -0,0 +1,11 @@
+class QuestionExplanation extends StatelessWidget {
+  const QuestionExplanation({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:
+      
+    );
+  }
+}
