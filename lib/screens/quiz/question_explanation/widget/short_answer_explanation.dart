0e212be0cc839f5ebb10e9a8cc6fe1feb6db1293
Alex <alexandre.marleau2000@gmail.com>
2021-08-19 12:31:54 -0400

    Question Explanation

--- lib/screens/quiz/question_explanation/widget/short_answer_explanation.dart
+++ lib/screens/quiz/question_explanation/widget/short_answer_explanation.dart
@@ -2,6 +2,7 @@ import 'package:demo3/model/question.dart';
 import 'package:demo3/model/question_attempt.dart';
 import 'package:demo3/model/quiz_attempt.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ShortAnswerExplanation extends StatelessWidget {
   final QuizAttempt _quizAttempt;
@@ -15,9 +16,18 @@ class ShortAnswerExplanation extends StatelessWidget {
     return Container(
       child: Column(
         children: [
-          Text("You have answered Wrong"),
+          Text(
+            "You have answered Wrong",
+            style: TextStyle(fontSize: 25, color: Colors.red),
+          ),
           Text("The question Was: "),
-          Text(_quizAttempt.questions[index].content),
+          Container(
+              height: MediaQuery.of(context).size.height / 5,
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.only(left: 40, right: 40),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.black, width: 2)),
+              child: Align(alignment: Alignment.center, child: Text(_quizAttempt.questions[index].content))),
           Text("your answer: " + _quizAttempt.questionAttempts[index].answer),
           Text("The good answer is " + _quizAttempt.questions[index].shortAnswers!.first.answerText.toString()),
           Padding(
