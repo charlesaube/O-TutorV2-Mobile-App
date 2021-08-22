f6ae7ac942c7c1c0863e0fe170ca3a823da54afc
Alex <alexandre.marleau2000@gmail.com>
2021-08-22 11:31:36 -0400

    Always Shown Scrollbar

--- lib/screens/quiz/quiz_process/widgets/multiple_choice.dart
+++ lib/screens/quiz/quiz_process/widgets/multiple_choice.dart
@@ -1,5 +1,6 @@
 import 'package:demo3/model/answer.dart';
 import 'package:demo3/model/question.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 typedef void VoidCallback(Answer newAnswer);
@@ -16,55 +17,61 @@ class MultipleChoice extends StatefulWidget {
 class _MultipleChoiceState extends State<MultipleChoice> {
   Color _colorContainer = Colors.blue;
   int _clicked = -1;
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 330,
       height: MediaQuery.of(context).size.width / 1.5,
-      child: ListView.builder(
-        itemCount: this.widget.question.multipleAnswers!.length,
-        itemBuilder: (
-          BuildContext context,
-          int index,
-        ) {
-          //Change la couleur du container ClickÃ© ------------------------
-          print(_clicked);
-          _colorContainer = Colors.grey.shade200;
-          if (index == _clicked) _colorContainer = Colors.orange;
+      child: Scrollbar(
+        controller: _scrollController,
+        isAlwaysShown: true,
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: this.widget.question.multipleAnswers!.length,
+          itemBuilder: (
+            BuildContext context,
+            int index,
+          ) {
+            //Change la couleur du container ClickÃ© ------------------------
+            print(_clicked);
+            _colorContainer = Colors.grey.shade200;
+            if (index == _clicked) _colorContainer = Colors.orange;
 
-          return Container(
-            height: 50,
-            margin: EdgeInsets.all(15),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: _colorContainer, width: 3),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              elevation: 6,
-              child: InkWell(
-                splashColor: Colors.lightBlue.withAlpha(50),
-                onTap: () {
-                  setState(() {
-                    _clicked = index;
-                  });
-                  widget.setAnswerCallback(this.widget.question.multipleAnswers![index]);
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      this.widget.question.multipleAnswers![index].answer,
-                      style: TextStyle(
-                        fontSize: 15,
+            return Container(
+              height: 50,
+              margin: EdgeInsets.all(15),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: _colorContainer, width: 3),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                elevation: 6,
+                child: InkWell(
+                  splashColor: Colors.lightBlue.withAlpha(50),
+                  onTap: () {
+                    setState(() {
+                      _clicked = index;
+                    });
+                    widget.setAnswerCallback(this.widget.question.multipleAnswers![index]);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        this.widget.question.multipleAnswers![index].answer,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
