dbc50e53d8e9a37384713d9be279dc7c0af712f3
Alex <alexandre.marleau2000@gmail.com>
2021-08-22 10:51:04 -0400

    Question Explanation

--- lib/screens/quiz/question_explanation/widget/short_answer_explanation.dart
+++ lib/screens/quiz/question_explanation/widget/short_answer_explanation.dart
@@ -25,27 +25,68 @@ class ShortAnswerExplanation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height / 1.1,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text(
-            "You have answered Wrong",
-            style: TextStyle(fontSize: 25, color: Colors.red),
-          ),
-          Text("The question Was: "),
           Container(
-              height: MediaQuery.of(context).size.height / 5,
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.only(left: 40, right: 40),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.black, width: 2)),
-              child: Align(alignment: Alignment.center, child: Text(_question.content))),
-          Text("your answer: " + fetchUserAnswer()),
-          Text("The good answer is " + _question.shortAnswers!.first.answerText.toString()),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Text(
+                  "You have answered Wrong",
+                  style: TextStyle(fontSize: 25, color: Colors.red),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height / 5,
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.only(left: 40, right: 40, top: 20),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                      side: BorderSide(
+                        color: Colors.lightBlue,
+                        width: 2.0,
+                      ),
+                    ),
+                    elevation: 10,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(_question.content),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          Text(
+            "Your answer: " + fetchUserAnswer(),
+            style: TextStyle(fontSize: 17),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 15, right: 15),
             child: Text(
-              _question.explanation,
-              textAlign: TextAlign.justify,
+              "The good answer is " + _question.shortAnswers!.first.answerText.toString(),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 17),
+            ),
+          ),
+          Spacer(),
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            margin: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.orange, width: 2.5), borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  _question.explanation,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
             ),
           ),
         ],
